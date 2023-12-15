e6410a696b94a09c1dd55d3925259eef96819e9d
Florian Chapoullie-Pino <florian.chapoullie-pino@etu.u-bordeaux.fr>
2023-12-15 09:50:04 +0100

    Changement de la page à propos, carousel à prévoir

--- JS/changementFond.js
+++ JS/changementFond.js
@@ -3,10 +3,10 @@ const banniere = document.querySelector('.HautPage');
 window.addEventListener('scroll', () => {
     const {scrollTop, clientHeight} = document.documentElement;
 
-    if (scrollTop > 750) {
+    if (scrollTop > 840) {
       banniere.classList.add('active')
     }
-    if (scrollTop < 750) {
+    if (scrollTop < 840) {
         banniere.classList.remove('active')
     }
     console.log(scrollTop)
